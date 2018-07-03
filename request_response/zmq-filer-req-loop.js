de4dea5e7fadbf2a1f345aa2b382ea5b432f1fab
Manoel Rui <manoel.rui@lsbd.ufc.br>
2018-07-03 12:26:52 -0300

    Added rep/req socket patterns

---
+++ request_response/zmq-filer-req-loop.js
@@ -0,0 +1,21 @@
+'use strict';
+const
+    zmq = require('zeromq'),
+    filename = process.argv[2],
+    // create request endpoint
+    requester = zmq.socket('req');  
+
+// handle replies from responder
+requester.on("message", function(data) {
+    let response = JSON.parse(data);
+    console.log("Received response:", response);
+});
+requester.connect("tcp://localhost:5433");
+
+for (let i=1; i<=3; i++) {
+    // send request for content
+    console.log('Sending request for ' + filename);
+    requester.send(JSON.stringify({
+        path: filename
+    }));
+}
